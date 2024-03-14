1b8519e5423d08862e8658eb832d81120aa0e869
bosscht <124249220+bosscht@users.noreply.github.com>
2024-03-15 02:30:18 +0700

    Up H7 P3-7

--- MLf/H7/P3.py
+++ MLf/H7/P3.py
@@ -1,23 +1,46 @@
-import numpy as np
 import pickle
 
-with open("P3_data.pkl", "rb") as f:
-    data = pickle.load(f)
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import PolynomialFeatures
 
-x_train = data["X"]
-y_train = data["Y"]
-degree = 14
 
-coefficients = np.polyfit(x_train, y_train, degree)
+# Load the data from 'P3_data.pkl'
+def load_data():
+    with open("P3_data.pkl", "rb") as fh:
+        loaded_data = pickle.load(fh)
+
+    xs = loaded_data["X"]
+    ys = loaded_data["Y"]
+
+    return xs, ys
 
 
 def DPolyPredict(x):
-    y_pred = np.polyval(coefficients, x)
+    xs, ys = load_data()
+    x_train, xtest, y_train, ytest = train_test_split(xs, ys, test_size=0.30)
+
+    model = fit_quadratic_model(x_train, y_train)
+    x = np.array(x).reshape(-1, 1)
+    y_pred = model.predict(x)
+
     return y_pred
+
+
+# make a quadratic model
+def fit_quadratic_model(xt, yt):
+    model = make_pipeline(PolynomialFeatures(degree=7), LinearRegression())
+    model.fit(xt.reshape(-1, 1), yt)
+    return model
 
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
+
+    xs, ys = load_data()
+    x_train, xtest, y_train, ytest = train_test_split(xs, ys, test_size=0.30)
 
     plt.plot(x_train, y_train, "k.")
     xp = np.linspace(0, 3, 200)
--- MLf/H7/P7.py
+++ MLf/H7/P7.py
@@ -1,36 +1,31 @@
 import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import StandardScaler
 
 
-def eDiag(X):
+def eDiag(x):
     factors = np.load("P7_x.npy")
     labels = np.load("P7_y.npy")
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        factors, labels, test_size=0.2, random_state=42
-    )
+    labels = np.ravel(labels)
+    # Split the data into training and testing sets
 
-    scaler = StandardScaler()
-    X_train_scaled = scaler.fit_transform(X_train)
-    X_test_scaled = scaler.transform(X_test)
+    # Build a binary classification model
+    model = RandomForestClassifier()
+    model.fit(factors, labels)
 
-    model = LogisticRegression(random_state=42)
+    # Make predictions on the provided data
+    predictions = model.predict(x)
 
-    model.fit(X_train_scaled, y_train)
-
-    predictions = model.predict(X)
     return predictions
 
 
+# Test the eDiag function
 if __name__ == "__main__":
     x = np.array([[5, 65, 4, 5, 3], [4, 72, 1, 3, 3], [2, 32, 2, 3, 3]])
 
     y = eDiag(x)
     print("Prediction:", y)
-
     Words = ["negative", "positive"]
-
     for i, yi in enumerate(y):
         print(f"Patient {i}'s result is {Words[yi]}.")
